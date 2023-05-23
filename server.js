62bfc3e141601c9c3caefcf08c73cd5cdcf9b489
mkg101 <manishkumargupta875@gmail.com>
2023-05-23 16:38:42 +0530

    api start

--- server.js
+++ server.js
@@ -1,64 +1,32 @@
 const express = require("express");
-const data = require("./users");
-const users = data.users;
 const { v4: uuid } = require("uuid");
+const mongoose = require("mongoose");
+const User = require("./user");
+mongoose
+  .connect(process.env.DBURI)
+  .then(() => console.log("connected"))
+  .catch((err) => console.log("error", err));
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  const { search } = req.query;
-  if (search) {
-    const matches = users.filter((user) => user.lname === search);
-    res.json(matches);
-  } else {
-    res.json(users);
-  }
+app.get("/users", async (req, res) => {
+  const users = await User.find();
+  res.json(users);
 });
 
-app.get("/users/:userId", (req, res) => {
-  const { userId } = req.params;
-  console.log(users);
-  const user = users.find((user) => user.id === userId);
-  if (user) {
-    res.json([user]);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const { fname, lname } = req.body;
-  const user = {
+  const user = await User.create({
     id: uuid(),
     fname,
     lname,
-  };
-  users.push(user);
+  });
+  await user.save();
+
   res.status(201).send(user);
 });
 
-app.put("/users/:userId", (req, res) => {
-  const { userId } = req.params;
-  const { fname, lname } = req.body;
-  let user = users.find((user) => user.id === userId);
-  if (user) {
-    user.fname = fname;
-    user.lname = lname;
-    res.json(user);
-  } else {
-    res.sendStatus(404);
-  }
-});
-
-app.delete("/users/:userId", (req, res) => {
-  const { userId } = req.params;
-  let id = users.findIndex((user) => user.id === userId);
-  if (id >= 0) {
-    users.splice(id, 1);
-  }
-  res.json(users);
-});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
